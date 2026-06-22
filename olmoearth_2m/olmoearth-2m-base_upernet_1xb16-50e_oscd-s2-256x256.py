caf503957c92615a47ca154a0aa179d02c8c4918
wangjieh <2394834585@qq.com>
2026-06-22 13:13:42 +0800

    test

---
+++ olmoearth_2m/olmoearth-2m-base_upernet_1xb16-50e_oscd-s2-256x256.py
@@ -0,0 +1,43 @@
+_base_ = [
+    './olmoearth_2m_upernet_OSCD.py',
+    './oscd_s2_256.py',
+    './default_runtime.py'
+]
+
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=50, val_interval=1)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+log_processor = dict(by_epoch=True)
+randomness = dict(seed=0)
+work_dir = "/mnt/qh2-nas3/EO_test/wry/work_dirs/olmoearth-2m-base_upernet_1xb16-50e_oscd-s2-256x256"
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(type='AdamW', lr=1e-3, weight_decay=0.01))
+
+param_scheduler = [
+    dict(
+        type='OneCycleLR',
+        eta_max=1e-3,
+        pct_start=0.0,
+        anneal_strategy='cos',
+        begin=0,
+        end=50,
+        by_epoch=True,
+        convert_to_iter_based=True)
+]
+
+default_hooks = dict(
+    checkpoint=dict(
+        type='CheckpointHook',
+        by_epoch=True,
+        interval=1,
+        max_keep_ckpts=1,
+        save_best='mIoU',
+        rule='greater',
+        save_last=True),
+    logger=dict(type='LoggerHook', interval=10, log_metric_by_epoch=True),
+    visualization=dict(
+        type='OlmoEarthOSCDVisualizationHook',
+        interval=1,
+        draw=True,
+        show=False))
